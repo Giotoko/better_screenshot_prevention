b7c6322e68eec19bcaea04ee5a318a23603e111c
Jhonaiquel Rodríguez <46872129+Giotoko@users.noreply.github.com>
2025-01-30 22:52:27 -0400

    Android

--- android/src/main/kotlin/com/jhonaiquel/better_screenshot_prevention/BetterScreenshotPreventionPlugin.kt
+++ android/src/main/kotlin/com/jhonaiquel/better_screenshot_prevention/BetterScreenshotPreventionPlugin.kt
@@ -7,14 +7,16 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import android.view.WindowManager.LayoutParams
+import android.app.Activity
 
 /** BetterScreenshotPreventionPlugin */
-class BetterScreenshotPreventionPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
+class BetterScreenshotPreventionPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
+
   private lateinit var channel : MethodChannel
+  private var activity: Activity? = null
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "better_screenshot_prevention")
@@ -22,14 +24,39 @@ class BetterScreenshotPreventionPlugin: FlutterPlugin, MethodCallHandler {
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+
+    when(call.method){
+      "preventScreenshot" -> result.success(preventScreenshot())
+      "allowScreenshot" -> result.success(allowScreenshot())
+      else -> result.notImplemented()
     }
   }
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
   }
+
+override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    activity = binding.activity
+}
+
+override fun onDetachedFromActivityForConfigChanges() {
+}
+
+override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    activity = binding.activity
+}
+
+override fun onDetachedFromActivity() {}
+
+  fun preventScreenshot () : Boolean {
+    activity?.getWindow()?.addFlags(LayoutParams.FLAG_SECURE)
+    return true
+  }
+
+  fun allowScreenshot () : Boolean {
+    activity?.getWindow()?.clearFlags(LayoutParams.FLAG_SECURE)
+    return true
+  }
+
 }
