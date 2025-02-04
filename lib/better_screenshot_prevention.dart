43a2d0f9ffc2cdd79a06ff60b5729a9de791737d
Jhonaiquel Rodríguez <46872129+Giotoko@users.noreply.github.com>
2025-02-03 21:37:42 -0400

    added docs and license

--- lib/better_screenshot_prevention.dart
+++ lib/better_screenshot_prevention.dart
@@ -1,17 +1,21 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+/// Defines the MethodChannel.
+///
+/// Defines the calls to the native functionality
 class BetterScreenshotPrevention {
-  static const MethodChannel _channel = MethodChannel('better_screenshot_prevention');
+  static const MethodChannel _channel =
+      MethodChannel('better_screenshot_prevention');
 
-  static Future<bool> get preventScreenshot async{
+  /// this funcion hides sensitive data by adding a black ovberlay to the screenshots
+  static Future<bool> get preventScreenshot async {
     return await _channel.invokeMethod("preventScreenshot");
   }
 
-  static Future<bool> get allowScreenshot async{
+  /// this function disables the back overlay and allows to take screenshots normally
+  static Future<bool> get allowScreenshot async {
     return await _channel.invokeMethod("allowScreenshot");
   }
-
 }
--- test/better_screenshot_prevention_test.dart
+++ test/better_screenshot_prevention_test.dart
@@ -1,22 +1,6 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:better_screenshot_prevention/better_screenshot_prevention.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('better_screenshot_prevention');
-
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
-
   test('getPlatformVersion', () async {
     expect('42', '42');
   });
